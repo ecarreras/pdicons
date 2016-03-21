17e702987d5de11f33712e587fde482de1a0c729
Jonathan la Cour <jon@lacour.me>
2016-03-21 13:15:13 -0600

    Improve cached icon handling to support browser navigation

--- static/script.js
+++ static/script.js
@@ -2,8 +2,10 @@ function iconLoadListener() {
     var icons = document.querySelectorAll(".icon__preview");
 
     Array.prototype.forEach.call(icons, function(icon, i) {
+        icon.src += "#" + Date.now().toString();
+
         icon.onload = function() {
-            this.parentNode.parentNode.className += "__loaded";
+            this.parentNode.parentNode.className = "icon__loaded";
         };
     });
 }
